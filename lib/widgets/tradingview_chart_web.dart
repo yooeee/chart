54bc6e84a5f07a8611476d05d2385f02a94e21ad
유은철 <81167605+yooeee@users.noreply.github.com>
2026-08-26 09:48:25 +0900

    fix: migrate TradingView DOM embed to package web

--- lib/widgets/tradingview_chart_web.dart
+++ lib/widgets/tradingview_chart_web.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-import 'dart:html' as html;
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
 
 /// TradingView's free Advanced Chart widget embedded as a Flutter Web
 /// platform view. The widget loads the market feed in the browser, so no
@@ -31,18 +31,18 @@ class _TradingViewChartState extends State<TradingViewChart> {
     super.initState();
     _viewType = 'tradingview-advanced-chart-${_nextViewId++}';
     ui_web.platformViewRegistry.registerViewFactory(_viewType, (int _) {
-      final container = html.DivElement()
+      final container = web.HTMLDivElement()
         ..className = 'tradingview-widget-container'
         ..style.width = '100%'
         ..style.height = '100%'
         ..style.backgroundColor = '#ffffff';
 
-      final chart = html.DivElement()
+      final chart = web.HTMLDivElement()
         ..className = 'tradingview-widget-container__widget'
         ..style.width = '100%'
         ..style.height = 'calc(100% - 28px)';
 
-      final copyright = html.DivElement()
+      final copyright = web.HTMLDivElement()
         ..className = 'tradingview-widget-copyright'
         ..style.height = '28px'
         ..style.display = 'flex'
@@ -50,13 +50,14 @@ class _TradingViewChartState extends State<TradingViewChart> {
         ..style.fontSize = '11px'
         ..style.color = '#737881';
 
-      final link = html.AnchorElement(href: 'https://www.tradingview.com/')
+      final link = web.HTMLAnchorElement()
+        ..href = 'https://www.tradingview.com/'
         ..target = '_blank'
         ..rel = 'noopener nofollow'
         ..text = 'Chart by TradingView';
-      copyright.children.add(link);
+      copyright.append(link);
 
-      final script = html.ScriptElement()
+      final script = web.HTMLScriptElement()
         ..type = 'text/javascript'
         ..src = 'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js'
         ..async = true
@@ -76,9 +77,9 @@ class _TradingViewChartState extends State<TradingViewChart> {
           'support_host': 'https://www.tradingview.com',
         });
 
-      container.children.add(chart);
-      container.children.add(copyright);
-      container.children.add(script);
+      container.append(chart);
+      container.append(copyright);
+      container.append(script);
       return container;
     });
   }
